6ef0b036b842367ed4a510a3b6c072623daf32af
panickylemon <alenka__1992@mail.ru>
2016-04-06 00:36:49 +0300

    4 trigger

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
     $('.bxslider').bxSlider({
-        mode: 'horizontal',           // тип перехода между слайдами может быть 'horizontal', 'vertical', 'fade'
-        easing: 'easeInOutQuad',// анимация слайда
+        mode: 'horizontal',     // тип перехода между слайдами может быть 'horizontal', 'vertical', 'fade'
         controls: true,         // отображение стрелки - вперед, назад
         startSlide: 0,          // Показ начнется с заданного слайда
         infiniteLoop: true,     // показывать первый слайд за последним
         pager: true,            // показ номера страницы
-        auto: false,             // сделать автоматический переход
-        autoControls: false,     // показывает кнопку плей и стоп
+        auto: true,            // сделать автоматический переход
+        autoControls: false,    // показывает кнопку плей и стоп
         pause: 4000,            // время между сменой слайдов в м-сек
-        speed: 1000,             // длительность перехода слайда в м-сек
+        speed: 1000,            // длительность перехода слайда в м-сек
         adaptiveHeight: true
     });
 
